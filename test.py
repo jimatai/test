b6946aac9ae199d5d07504f897b969c0afc192a6
jimatai <you@example.com>
2024-11-30 02:22:19 +0900

    sw

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ def main():
         st.write(transcription)
 
         service_account_info = st.secrets["dia"]["service_account_info"]
+        st.write(service_account_info)
         service_account_info = json.loads(service_account_info)
         session_id = uuid.uuid4()
         location = 'asia-northeast1'
